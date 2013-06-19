05d4ac98668a15d2339f68a4295a7eb9aa8eb8a9
Charmander <campersander@gmail.com>
2013-06-19 07:08:58 -0700

    Added support for rendering from stdin.
    Passing - as a file name to leaf will now read a Razor Leaf template
    from standard input and render it to standard output.

--- cli.js
+++ cli.js
@@ -6,17 +6,22 @@ var fs = require("fs");
 var path = require("path");
 var razorleaf = require("./razorleaf");
 
-for(var i = 2; i < process.argv.length; i++) {
-	var file = process.argv[i];
+function render(input, outputFile, directory, callback) {
+	var buffers = [];
 
-	fs.readFile(file, "utf8", function(error, content) {
-		if(error) {
-			console.error(error);
-			process.exit(1);
+	input.on("error", callback);
+	output.on("error", callback);
+
+	input.on("readable", function() {
+		var part = input.read();
+
+		if(part) {
+			buffers.push(part);
 		}
+	});
 
-		var directory = path.dirname(path.resolve(file));
-		var outputFile = path.join(directory, path.basename(file, ".leaf") + ".html");
+	input.on("end", function() {
+		var content = Buffer.concat(buffers).toString("utf8");
 
 		var template = razorleaf.compile(content, {
 			include: function(name) {
@@ -24,18 +29,51 @@ for(var i = 2; i < process.argv.length; i++) {
 			}
 		});
 
-		fs.writeFile(outputFile, template(), {
-			encoding: "utf8",
-			mode: 0x180
-		}, function(error) {
-			if(error) {
-				console.error(error);
-				process.exit(1);
-			}
-
-			console.log("Rendered %s as %s", file, outputFile);
+		output.write(template(), function() {
+			callback(null, output);
 		});
 	});
+}
+
+function renderedStdin(error) {
+	if(error) {
+		console.error(error);
+		process.exit(1);
+	}
+}
+
+function renderedFile(error, output) {
+	output.close();
+
+	if(error) {
+		console.error(error);
+		process.exit(1);
+	}
+}
+
+for(var i = 2; i < process.argv.length; i++) {
+	var file = process.argv[i];
+	var input;
+	var output;
+	var directory;
+	var callback;
+
+	if(file === "-") {
+		directory = process.cwd();
+		input = process.stdin;
+		output = process.stdout;
+		callback = renderedStdin;
+	} else {
+		directory = path.dirname(path.resolve(file));
+		input = fs.createReadStream(file);
+
+		var outputFile = path.join(directory, path.basename(file, ".leaf") + ".html");
+		output = fs.createWriteStream(outputFile, {mode: 0x180});
+		callback = renderedFile;
+		console.error("Rendering %s as %s", file, outputFile);
+	}
+
+	render(input, output, directory, callback);
 }
 
 // vim:ft=javascript
